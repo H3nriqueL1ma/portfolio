7ba3b6a99a8497de33ab8f7d1bea9797b737d548
HENRIQUE CARLOS COSTA DE LIMA <henrique.cclima@senacsp.edu.br>
2024-05-08 22:13:41 -0300

    Mudança do código do index para componentes

---
+++ src/components/Init.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function InitEntrySystem() {
+    return (
+        <>
+            <div 
+                id="main" 
+            >
+                <div 
+                    id="img" 
+                >
+                    <img src="Logo-Portfolio.png"/>
+                </div>
+                <div class="p-2">
+                    <p 
+                        id="name-bios" 
+                    >
+                        Henrique Lima (C)2024.
+                    </p>
+                    <p 
+                        id="memory" 
+                    >
+                        Memory Testing... 
+                    </p>
+                    <p 
+                        id="skills" 
+                    >
+                        Analyzing Skills and Projects...
+                    </p>
+                    <p 
+                        id="learnings" 
+                    >
+                        Optimizing Learnings...
+                    </p>
+                    <p 
+                        id="started-portfolio" 
+                    >
+                        [ <span>OK</span> ] Started Portfolio.
+                    </p>
+                    <p 
+                        id="modules" 
+                    >
+                        [ <span>OK</span> ] Started Modules.
+                    </p>
+                    <p 
+                        id="graphic" 
+                    >
+                        [ <span>OK</span> ] Started Graphic Interface.
+                    </p>
+                    <p 
+                        id="starting-portfolio" 
+                    >
+                        Starting Portfolio...
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
