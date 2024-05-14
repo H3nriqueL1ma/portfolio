9f1a84ff69e7a05a08e4b9ddd8910fdc4268eef4
HENRIQUE CARLOS COSTA DE LIMA <henrique.cclima@senacsp.edu.br>
2024-05-13 21:41:17 -0300

    Criação do formulário e finalização do front-end (por enquanto)

--- src/components/CardContents.jsx
+++ src/components/CardContents.jsx
@@ -6,11 +6,13 @@ import { React, useState, useEffect } from "react";
 export function MainAfterContent() {
     const [showMain, setShowMain] = useState(false);
     const [showMain2, setShowMain2] = useState(false);
+    const [showMain3, setShowMain3] = useState(false);
 
     useEffect(() => {
         const home = document.querySelector("#home");
         const about = document.querySelector("#sobre");
         const portfolio = document.querySelector("#portfolio");
+        const contact = document.querySelector("#contato");
 
         const elements = document.querySelectorAll(".fade-in-2");
         const elementsInline = document.querySelectorAll(".fade-in-2-inline");
@@ -18,6 +20,7 @@ export function MainAfterContent() {
         home.addEventListener("click", () => {
             setShowMain(false);
             setShowMain2(false);
+            setShowMain3(false);
             elements.forEach((el) => {
                 el.style.display = "block";
                 el.style.opacity = 1;
@@ -30,6 +33,7 @@ export function MainAfterContent() {
 
         about.addEventListener("click", () => {
             setShowMain2(false);
+            setShowMain3(false);
             elements.forEach((el) => {
                 el.style.display = "none";
                 el.style.opacity = 0;
@@ -39,19 +43,11 @@ export function MainAfterContent() {
                 el.style.opacity = 0;
             });
             setShowMain(true);
-
-            setTimeout(() => {
-                elements.forEach((el) => {
-                    el.classList.add("fade-transition"); // Adicionar classe para aplicar transição suave
-                });
-                elementsInline.forEach((el) => {
-                    el.classList.add("fade-transition"); // Adicionar classe para aplicar transição suave
-                });
-            }, 50);
         });
 
         portfolio.addEventListener("click", () => {
             setShowMain(false);
+            setShowMain3(false);
             elements.forEach((el) => {
                 el.style.display = "none";
                 el.style.opacity = 0;
@@ -61,15 +57,6 @@ export function MainAfterContent() {
                 el.style.opacity = 0;
             });
             setShowMain2(true);
-            
-            setTimeout(() => {
-                elements.forEach((el) => {
-                    el.classList.add("fade-transition"); // Adicionar classe para aplicar transição suave
-                });
-                elementsInline.forEach((el) => {
-                    el.classList.add("fade-transition"); // Adicionar classe para aplicar transição suave
-                });
-            }, 50);
 
             const imgOverlay1 = document.querySelector("#overlay-1");
             const imgOverlay2 = document.querySelector("#overlay-2");
@@ -104,6 +91,20 @@ export function MainAfterContent() {
                 });
             }
         });
+
+        contact.addEventListener("click", () => {
+            setShowMain(false);
+            setShowMain2(false);
+            elements.forEach((el) => {
+                el.style.display = "none";
+                el.style.opacity = 0;
+            });
+            elementsInline.forEach((el) => {
+                el.style.display = "none";
+                el.style.opacity = 0;
+            });
+            setShowMain3(true);
+        });        
     }, [])
 
     return (
@@ -137,9 +138,9 @@ export function MainAfterContent() {
                 <div id="title" style={ {padding: "1rem 5rem"} }>
                     <h3 className="line typing-animation" style={ {color: "white"} }>Henrique Lima GRUB 0.1.0</h3>
                 </div>
-                <div id="card" className="pt-3 pb-3 pe-2 ps-2 text-center align-content-center justify-content-center">
+                <div id="card" className="pt-3 pb-3 pe-2 ps-2 text-center align-content-center justify-content-center fade-transition">
                     <h1 className="fade-in-2">Henrique Lima</h1>
-                    <h2 className="fade-in-2">Desenvolvedor Back-end e Baixo Nível.</h2>
+                    <h2 id="subtitle" className="fade-in-2 pt-1">Desenvolvedor Back-end e Baixo Nível.</h2>
                     <a href="https://github.com/H3nriqueL1ma" target="_blank" rel="noreferrer noopener" className="fade-in-2-inline">
                         <i className="bi bi-github"></i>
                     </a> 
@@ -148,6 +149,7 @@ export function MainAfterContent() {
                     </a> 
                     {showMain && <AboutContent />}
                     {showMain2 && <PortfolioContent />}
+                    {showMain3 && <ContactContent />}
                 </div>
             </div>
         </>
@@ -162,47 +164,47 @@ function AboutContent() {
                 <p style={ {margin: "10px 5px"} }>Sou <span style={ {color: "rgb(46, 46, 221)"} }>desenvolvedor back-end</span>, ainda sou um iniciante na área e não possuo experiência profissional, mas sempre estou estudando as tecnologias mais utilizadas no mercado. Além disso, nos tempos vagos sou desenvolvedor de baixo nível por hobby.</p>
                 <h4 className="m-0 mt-4 h4-about">Escolaridade</h4>
                 <p style={ {margin: "10px 5px"} }>
-                    <u>Dez_2022 - Atualmente</u> <i class="bi bi-calendar"></i><br/>
+                    <u>Dez_2022 - Atualmente</u> <i className="bi bi-calendar"></i><br/>
                     Aprimorei meus conhecimentos e ainda continuo aprimorando com conteúdos disponíveis na internet, como cursos online, documentações e livros.
                 </p>
                 <p style={ {margin: "10px 5px"} }>
-                    <u>Fev_2024 - Atualmente</u> <i class="bi bi-calendar"></i><br/>
+                    <u>Fev_2024 - Atualmente</u> <i className="bi bi-calendar"></i><br/>
                     Comecei uma formação técnica em Informática para a Internet pelo SENAC. Onde estou estudando desenvolvimento web fullstack.
                 </p>
                 <h4 className="m-0 mt-4 h4-about">Conhecimento Técnico</h4>
-                <p id="techs-content" className="d-flex justify-content-center" style={ {margin: "10px 5px"} }>
-                    <div className="text-center icons">      
-                        <i class="devicon-javascript-plain font-icons"></i>
+                <p id="techs-content" className="row justify-content-center" style={ {margin: "10px 5px"} }>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-javascript-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-typescript-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-typescript-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-react-original font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-react-original font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-nextjs-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-nextjs-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-sass-original font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-sass-original font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-bootstrap-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-bootstrap-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-nodejs-plain-wordmark font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-nodejs-plain-wordmark font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-fastify-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-fastify-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-mysql-original font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-mysql-original font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-c-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-c-plain font-icons"></i>
                     </div>
-                    <div className="text-center icons">      
-                        <i class="devicon-java-plain font-icons"></i>
+                    <div className="text-center icons col-1">      
+                        <i className="devicon-java-plain font-icons"></i>
                     </div>
                 </p>
             </div>
@@ -258,9 +260,27 @@ function ContactContent() {
     return (
         <>
             <div>
-                <div>
-                    <h5>Telefone</h5>
-                </div>
+                <form>
+                    <div id="content-form" className="text-start">
+                        <h3>Entre em contato!</h3><br/>
+                        <div>
+                            <label htmlFor="name">Seu Nome</label><br/>
+                            <input type="text" id="html" placeholder="Qual seu nome?"/>
+                        </div>
+                        <div>
+                            <label htmlFor="name">Seu E-mail</label><br/>
+                            <input type="text" id="html" placeholder="Qual seu e-mail?"/>
+                        </div>
+                        <div>
+                            <label htmlFor="name">Sua Mensagem</label><br/>
+                            <textarea rows={4} cols={29} type="text" id="html" placeholder="Qual sua mensagem?"/>
+                        </div>
+
+                        <div className="text-end mt-5">
+                            <button>Enviar</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </>
     );
