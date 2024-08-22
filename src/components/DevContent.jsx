5c621a121ea6d1524dce9662540d015fe0e0cea8
H3nriqueL1ma <henriquecarlos361893@gmail.com>
2024-08-22 15:44:52 -0300

    fix: Conserto na quebra de linha dos icones das tecnologias

--- src/components/DevContent.jsx
+++ src/components/DevContent.jsx
@@ -10,10 +10,10 @@ export default function DevContent() {
                         <h5>Sobre mim</h5>
                         <i className="bi bi-arrow-down"></i>
                     </div>
-                    <div id="about-me-content">
+                    <div id="about-me-content" className="row m-0">
                         Sou um desenvolvedor backend iniciante focado em criar soluções eficientes e escaláveis. Minhas principais stacks são Java, Spring, Docker e PostgreSQL. Gosto de trabalhar em ambientes Linux, utilizando o terminal para otimizar meu fluxo de trabalho. Atualmente, estou desenvolvendo projetos que envolvem APIs, microsserviços, arquitetura MVC e princípios SOLID, buscando sempre aprender e aplicar novas tecnologias.
 
-                        <div id="techs" className="mt-5">
+                        <div id="techs" className="col-lg-12 mt-5 d-flex flex-wrap justify-content-center gap-2">
                             <i className="devicon-java-plain"></i>
                             <i className="devicon-typescript-plain"></i>
                             <i className="devicon-spring-original"></i>
